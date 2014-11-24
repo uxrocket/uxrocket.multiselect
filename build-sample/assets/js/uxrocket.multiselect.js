d7d6e736db9cd6f0672cbea563455cf84feca215
Rameş Aliyev <creator@ramesaliyev.com>
2014-11-24 10:39:57 +0200

    First dirt commit of v0.1.0

---
+++ build-sample/assets/js/uxrocket.multiselect.js
@@ -0,0 +1,4 @@
+// Vendor
+//@codekit-append "../../../vendors/uxrocket.utils-with-lodash.min.js"
+//@codekit-append "../../../vendors/uxrocket.factory.js"
+//@codekit-append "../../../lib/uxrocket.multiselect.js"
